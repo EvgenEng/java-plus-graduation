c779a8fb294881edb63afc4ee737b2c99daa24d6
EvgenEng <zhenya-chaplygin@yandex.ru>
2026-01-10 18:47:15 +0300

    Fix:

--- infra/config-server/src/main/java/ru/practicum/ConfigServer.java
+++ infra/config-server/src/main/java/ru/practicum/ConfigServer.java
@@ -2,10 +2,15 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
 @EnableConfigServer
-@SpringBootApplication
 public class ConfigServer {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServer.class, args);
